4ab19a59dc4754baf1fb3030059bce5283cfeb8b
Pixel-Pop <fireturtonator@gmail.com>
2021-09-18 18:04:08 -0700

    Initial commit

---
+++ MouseRebind/KeyboardOptionsScreen.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using MonoMod;
+using UnityEngine;
+using Rewired;
+
+#pragma warning disable CS0626
+#pragma warning disable CS0649
+
+class patch_KeyboardOptionsScreen : KeyboardOptionsScreen
+{
+    /* Existing private fields */
+    [MonoModIgnore]
+    private int currentActionId;
+    [MonoModIgnore]
+    private Player player;
+	[MonoModIgnore]
+	private Pole? currentPole;
+
+	/* New fields */
+	private Mouse mouse;
+
+    // InputMapper.Default is the keyboard input mapper.
+    private InputMapper inputMapperMouse;
+
+	// For drawBinding 
+	ControllerMap keyboardMap;
+	ControllerMap mouseMap;
+	List<ActionElementMap> listMouse;
+
+    private string savePathMouse
+    {
+        get
+        {
+            if (PlatformManager.Instance != null)
+            {
+                return PlatformManager.Instance.getDataPath("VB_Mousebindings");
+            }
+            return Application.persistentDataPath + "\\VB_Mousebindings.xml";
+        }
+    }
+
+    private int elementMapIdToRemove;
+    private ControllerMap controllerMapOfRemove;
+
+    /* Methods */
+    public extern void orig_KeyboardOptionsScreen();
+    [MonoModConstructor]
+    public void KeyboardOptionsScreen()
+    {
+        inputMapperMouse = new InputMapper();
+        orig_KeyboardOptionsScreen();
+    }
+
+
+    private extern void orig_Start();
+    private void Start()
+    {
+        if (!assignableActionNames.Contains("Attack"))
+        {
+            assignableActionNames.Add("Attack");
+        }
+
+        orig_Start();
+
+        mouse = player.controllers.Mouse;
+
+        inputMapperMouse.options.ignoreMouseXAxis = true;
+        inputMapperMouse.options.ignoreMouseYAxis = true;
+        inputMapperMouse.InputMappedEvent += OnInputMapped;
+        inputMapperMouse.ConflictFoundEvent += OnConflictFound;
+    }
+
+    public void drawBindingFullBox(ActionElementMap item)
+    {
+        elementMapIdToRemove = item.id;
+        controllerMapOfRemove = item.controllerMap;
+
+        AxisRange actionRange = (currentPole.HasValue ? ((currentPole.GetValueOrDefault() == Pole.Positive && currentPole.HasValue) ? AxisRange.Positive : AxisRange.Negative) : AxisRange.Full);
+
+        InputMapper.Context mappingContextKey = new InputMapper.Context()
+        {
+            actionId = currentActionId,
+            controllerMap = keyboardMap,
+            actionRange = actionRange
+        };
+
+        InputMapper.Context mappingContextMouse = new InputMapper.Context()
+        {
+            actionId = currentActionId,
+            controllerMap = mouseMap,
+            actionRange = actionRange
+        };
+
+        if (item.controllerMap == keyboardMap)
+        {
+            mappingContextKey.actionElementMapToReplace = item;
+        } else
+        {
+            mappingContextMouse.actionElementMapToReplace = item;
+        }
+
+        InputMapper.Default.Start(mappingContextKey);
+        inputMapperMouse.Start(mappingContextMouse);
+    }
+
+    public void drawBindingEmptyBox()
+    {
+        AxisRange actionRange = (currentPole.HasValue ? ((currentPole.GetValueOrDefault() == Pole.Positive && currentPole.HasValue) ? AxisRange.Positive : AxisRange.Negative) : AxisRange.Full);
+
+        InputMapper.Default.Start(new InputMapper.Context()
+        {
+            actionId = currentActionId,
+            controllerMap = keyboardMap,
+            actionRange = actionRange
+        });
+        inputMapperMouse.Start(new InputMapper.Context()
+        {
+            actionId = currentActionId,
+            controllerMap = mouseMap,
+            actionRange = actionRange
+        });
+
+        controllerMapOfRemove = null;
+    }
+
+    [PatchDrawBinding]
+    public extern float orig_drawBinding(InputAction action, string actionName, Pole? pole, float y);
+    public new float drawBinding(InputAction action, string actionName, Pole? pole, float y)
+    {
+		keyboardMap = player.controllers.maps.GetMap(ControllerType.Keyboard, 0, "Default", "Default");
+		mouseMap = player.controllers.maps.GetMap(ControllerType.Mouse, 0, "Default", "Default");
+
+        listMouse = new List<ActionElementMap>();
+        player.controllers.maps.GetElementMapsWithAction(mouse, action.id, skipDisabledMaps: true, listMouse);
+
+        return orig_drawBinding(action, actionName, pole, y);
+	}
+
+    private extern void orig_OnInputMapped(InputMapper.InputMappedEventData data);
+    private void OnInputMapped(InputMapper.InputMappedEventData data)
+    {
+        InputMapper.Default.Stop();
+        inputMapperMouse.Stop();
+
+        if (controllerMapOfRemove != null)
+        {
+            if (controllerMapOfRemove != data.actionElementMap.controllerMap)
+            {
+                controllerMapOfRemove.DeleteElementMap(elementMapIdToRemove);
+            }
+        }
+
+        orig_OnInputMapped(data);
+    }
+
+    private extern void orig_OnConflictFound(InputMapper.ConflictFoundEventData data);
+    private void OnConflictFound(InputMapper.ConflictFoundEventData data)
+    {
+        orig_OnConflictFound(data);
+    }
+
+    private extern void orig_Load();
+    private void Load()
+    {
+        orig_Load();
+        if (File.Exists(savePathMouse))
+        {
+            TextReader textReader = new StreamReader(savePathMouse);
+            string item = textReader.ReadToEnd();
+            List<string> list = new List<string>();
+            list.Add(item);
+            player.controllers.maps.ClearMaps(ControllerType.Mouse, userAssignableOnly: true);
+            int num = player.controllers.maps.AddMapsFromXml(ControllerType.Mouse, 0, list);
+            textReader.Close();
+
+            Save();
+        }
+    }
+
+    private extern void orig_Save();
+    private void Save()
+    {
+        orig_Save();
+        string value = player.GetSaveData(userAssignableMapsOnly: true).mouseMapSaveData[0].keyboardMap.ToXmlString();
+        TextWriter textWriter2 = new StreamWriter(savePathMouse);
+        textWriter2.WriteLine(value);
+        textWriter2.Close();
+    }
+
+    private extern void orig_ClearSave();
+    private void ClearSave()
+    {
+		player.controllers.maps.LoadDefaultMaps(ControllerType.Mouse);
+		orig_ClearSave();
+        if (File.Exists(savePathMouse))
+        {
+            File.Delete(savePathMouse);
+        }
+    }
+
+    public extern void orig_OnExit(bool isBackNavigation);
+    public void OnExit(bool isBackNavigation)
+    {
+
+        if (inputMapperMouse.status == InputMapper.Status.Listening)
+        {
+            inputMapperMouse.Stop();
+            currentActionId = -1;
+        }
+        orig_OnExit(isBackNavigation);
+    }
+}
---
+++ MouseRebind/MonoModRules.cs
@@ -0,0 +1,109 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Mono.Cecil.Rocks;
+using MonoMod.Cil;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Rewired;
+
+
+namespace MonoMod
+{
+
+	[MonoModCustomMethodAttribute(nameof(MonoModRules.PatchDrawBinding))]
+	class PatchDrawBinding : Attribute { }
+
+	static class MonoModRules
+	{
+
+		static MonoModRules()
+		{
+			Console.WriteLine("Patching Void Bastards");
+		}
+
+		public static void PatchDrawBinding(MethodDefinition method, CustomAttribute attrib)
+		{
+			Console.WriteLine("Patching drawBinding");
+			if (method.HasBody)
+			{
+				ILCursor cursor = new ILCursor(new ILContext(method));
+				TypeDefinition type = method.DeclaringType;
+
+				FieldDefinition listMouse = type.Fields.First(tmp => tmp.Name.Equals("listMouse"));
+				MethodReference addRange = type.Module.ImportReference(typeof(List<ActionElementMap>).GetMethod("AddRange"));
+
+				/*
+				 * 	list.AddRange(listMouse);
+				 */
+
+				cursor.TryGotoNext(MoveType.After, instr => instr.MatchPop());
+				cursor.Emit(OpCodes.Ldloc_S, (byte)4); // Load list
+				cursor.Emit(OpCodes.Ldarg_0);
+				cursor.Emit(OpCodes.Ldfld, listMouse);
+				cursor.Emit(OpCodes.Callvirt, addRange);
+
+				MethodReference op_Equality = type.Module.ImportReference(typeof(string).GetMethod("op_Equality"));
+				MethodReference get_Item = type.Module.ImportReference(typeof(List<ActionElementMap>).GetMethod("get_Item"));
+				MethodReference get_elementIdentifierName = type.Module.ImportReference(typeof(ActionElementMap).GetMethod("get_elementIdentifierName"));
+				Instruction patchEnd = cursor.IL.Create(OpCodes.Ldloc_S, (byte)7);
+
+				/*
+				 *	if (safe == "None")
+				 *  {
+				 *		safe = list[i].elementIdentifierName;
+				 *	}
+				 */
+
+				cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdloc(7));
+				cursor.Emit(OpCodes.Ldstr, "None");
+				cursor.Emit(OpCodes.Call, op_Equality);
+				cursor.Emit(OpCodes.Brfalse_S, patchEnd);
+				cursor.Emit(OpCodes.Ldloc_S, (byte)4); // Load list
+				cursor.Emit(OpCodes.Ldloc_S, (byte)6); // Load i
+				cursor.Emit(OpCodes.Callvirt, get_Item);
+				cursor.Emit(OpCodes.Callvirt, get_elementIdentifierName);
+				cursor.Emit(OpCodes.Stloc_S, (byte)7); // Store safe
+				cursor.IL.InsertBefore(cursor.Next, patchEnd); // Load safe
+
+				/* Patch loop to show 3 columns instead of 2 */
+				cursor.TryGotoNext(MoveType.Before, instr => instr.OpCode == OpCodes.Blt);
+				cursor.Prev.OpCode = OpCodes.Ldc_I4_3;
+				cursor.TryGotoNext(MoveType.Before, instr => instr.OpCode == OpCodes.Blt);
+				cursor.Prev.OpCode = OpCodes.Ldc_I4_3;
+
+				/* Change behavior when empty box is clicked */
+				cursor.TryGotoPrev(MoveType.Before, instr => instr.MatchNewobj("Rewired.InputMapper/Context"));
+				while (cursor.Next.OpCode != OpCodes.Pop)
+                {
+					cursor.Remove();
+				}
+				cursor.Remove();
+				cursor.TryGotoNext(MoveType.After, instr => instr.MatchStfld("KeyboardOptionsScreen", "currentButtonId"));
+				cursor.Emit(OpCodes.Ldarg_0);
+				cursor.Emit(OpCodes.Callvirt, type.Methods.First(tmp => tmp.Name == "drawBindingEmptyBox"));
+
+				/* Change behavior when box with mapping is clicked */
+				cursor.TryGotoPrev(MoveType.Before, instr => instr.MatchNewobj("Rewired.InputMapper/Context")); // Other call was removed
+				while (cursor.Next.OpCode != OpCodes.Pop)
+				{
+					cursor.Remove();
+				}
+				cursor.Remove();
+				cursor.TryGotoNext(MoveType.After, instr => instr.MatchStfld("KeyboardOptionsScreen", "currentButtonId"));
+				cursor.Emit(OpCodes.Ldarg_0);
+				cursor.Emit(OpCodes.Ldloc_S, (byte)4); // Load list
+				cursor.Emit(OpCodes.Ldloc_S, (byte)6); // Load i
+				cursor.Emit(OpCodes.Callvirt, get_Item);
+				cursor.Emit(OpCodes.Callvirt, type.Methods.First(tmp => tmp.Name == "drawBindingFullBox"));
+
+				cursor.Body.Optimize();
+			}
+			else
+			{
+				Console.WriteLine("Error patching drawBinding");
+				throw new Exception();
+			}
+		}
+	}
+}
